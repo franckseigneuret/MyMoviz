ed599eddf23bf8e03647fd0e5a9fb53205ed7528
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-01 11:45:55 +0100

    ajout bootstrap (reactstrap) + début des premiers composants de la page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Nav, NavItem, NavLink,
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <img src="./img/logo.png" alt="" />
+        <Nav>
+          <NavItem>
+            <NavLink href="#">Last release</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">11 films</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <Card>
+            <CardImg top width="100%" src="/img/badboy3.jpg" alt="Card image cap" />
+            <CardBody>
+              <CardTitle tag="h5">Card title</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+      </div>
     </div>
   );
 }
