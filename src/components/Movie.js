9274271f04bb725e57c5b170c27ff0a1e86a2293
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-02 14:59:45 +0100

    ajout notation utilisateur

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, ButtonGroup, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons'
+
+import MyAdvice from './MyAdvice'
 
 const Movie = (props) => {
   const {
@@ -9,23 +11,32 @@ const Movie = (props) => {
     desc,
     img,
     note,
-    vote,
+    // vote,
     vue,
   } = props.datas
 
   const [likeMovie, setLikeMovie] = useState(false)
   const [watchMovie, setWatchMovie] = useState(false)
   const [countWatchMovie, setCountWatchMovie] = useState(vue)
+  const [myRatingMovie, setMyRatingMovie] = useState(0)
 
   const likeClass = likeMovie ? 'like' : ''
   const watchClass = watchMovie ? 'have-seen' : ''
-  
+
   const handleWatchClick = () => {
-    // console.log(watchMovie)
     setWatchMovie(!watchMovie)
-    // console.log(watchMovie)
     const toggle = watchMovie ? -1 : 1
     setCountWatchMovie(countWatchMovie + toggle)
+  }
+
+  const handleAdviceClick = (n) => {
+    if (n > 0 && myRatingMovie < 10) {
+      setMyRatingMovie(myRatingMovie + n)
+    }
+    if (n < 0 && myRatingMovie > 0) {
+      setMyRatingMovie(myRatingMovie + n)
+    }
+    console.log(myRatingMovie)
   }
 
   return (
@@ -33,19 +44,25 @@ const Movie = (props) => {
       <Card>
         <CardImg top width="100%" src={img} alt="Card image cap" />
         <CardBody>
-          <p>
+          <div>
             Like <FontAwesomeIcon icon={faHeart} className={likeClass} style={{ cursor: 'pointer' }} onClick={() => setLikeMovie(!likeMovie)} />
-          </p>
-          <p>
-            Nombre de vue <FontAwesomeIcon icon={faVideo} 
-              className={watchClass} onClick={() => handleWatchClick()} /> { countWatchMovie }
-          </p>
-          <p>
-            Mon avis <FontAwesomeIcon icon={faStar} /> <Button>+1</Button> <Button>-1</Button>
-          </p>
-          <p>
+          </div>
+          <div>
+            Nombre de vue <FontAwesomeIcon icon={faVideo}
+              className={watchClass} onClick={() => handleWatchClick()} /> {countWatchMovie}
+          </div>
+          <div>
+            Mon avis
+            <MyAdvice myRatingMovie={myRatingMovie} />
+            
+            <ButtonGroup size="sm">
+              <Button onClick={() => handleAdviceClick(-1)}>-1</Button>
+              <Button onClick={() => handleAdviceClick(1)}>+1</Button>
+            </ButtonGroup>
+          </div>
+          <div>
             Moyenne <FontAwesomeIcon icon={faStar} /> {note}
-          </p>
+          </div>
           <CardTitle tag="h5">{name}</CardTitle>
           <CardText>{desc}</CardText>
         </CardBody>
---
+++ src/components/MyAdvice.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons'
+
+const MyAdvice = ({ myRatingMovie }) => {
+  const stars = []
+  for (let i = 0; i < 10; i++) {
+    let style = 'yellow'
+    if (i >= myRatingMovie) {
+      style = ''
+    }
+
+    stars.push(<FontAwesomeIcon key={i} icon={faStar} className={style} />)
+  }
+
+  return stars
+}
+
+export default MyAdvice
