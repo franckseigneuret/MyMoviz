ba65892cc9118e75c93696f4bff9df197ce87ab3
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-02 11:36:11 +0100

    ajout hook useState pour gérer un like de film

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons'
 
 const Movie = (props) => {
-
   const {
     name,
     desc,
@@ -13,13 +12,21 @@ const Movie = (props) => {
     vote,
   } = props.datas
 
+  const [likeMovie, setLikeMovie] = useState(false)
+
+  const handleClickLike = () => {
+    setLikeMovie(!likeMovie)
+  }
+
+  const likeClass = likeMovie ? 'like' : ''
+
   return (
     <div className="col-12 col-lg-6 col-xl-4 movie-card">
       <Card>
         <CardImg top width="100%" src={img} alt="Card image cap" />
         <CardBody>
           <p>
-            Like <FontAwesomeIcon icon={faHeart} style={{cursor :'pointer'}} />
+            Like <FontAwesomeIcon icon={faHeart} className={likeClass} style={{cursor :'pointer'}} onClick={()=>handleClickLike()} />
           </p>
           <p>
             Nombre de vue <FontAwesomeIcon icon={faVideo} /> {vote}
